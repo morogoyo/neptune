bffebe94bbb83ad8b4e25ee61bab00e99bf9be4a
Rene Torres <morogoyo@gmail.com>
2017-09-24 07:44:37 -0400

    Update connection.php

--- working_scripts/connection.php
+++ working_scripts/connection.php
@@ -1,9 +1,9 @@
 <?php 
 // this connection file is for 192.168.22.4
-define( "HOST", "192.168.22.4");
-define( "USERNAME", "felix");
-define( "DATABASE", "data_temp");
-define( "PASSWORD", "Frt011290");
+define( "HOST", "*");
+define( "USERNAME", "*");
+define( "DATABASE", "*");
+define( "PASSWORD", "*");
 global $con;
 $con = new mysqli(HOST,USERNAME,PASSWORD,DATABASE);
 if (!$con){
